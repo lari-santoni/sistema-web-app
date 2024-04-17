0b4b0ab4f8bf55efeb7bba6b5ec15a40420d80f1
Larissa Santoni <lari.santoni@gmail.com>
2024-04-17 19:47:00 -0300

    fix front warning

--- src/app/pages/account/services/forms.service.ts
+++ src/app/pages/account/services/forms.service.ts
@@ -12,62 +12,74 @@ import { Characteristics, ReportResult } from '../models/report';
 export class FormsService {
 
   private readonly urlBase = environment.urlBase
+  token: any = null
 
   constructor(private http: HttpClient) { }
 
   getBasicInfo(id_student: string) {
-    const token = localStorage.getItem('token') || {}
+    if (typeof localStorage !== 'undefined'){
+      this.token = localStorage.getItem('token') || {}
+    }
+    
 
     let head = new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('Access-Control-Allow-Origin', '*')
-        .set('Authorization', `Bearer ${token}`)
+        .set('Authorization', `Bearer ${this.token}`)
 
     return this.http.get<any>(`${this.urlBase}/get-student?id=${id_student}`, { headers: head })
   }
 
   public getQuizQuestions(age: string): Observable<Quiz[]> {
 
-    const token = localStorage.getItem('token') || {}
+    if (typeof localStorage !== 'undefined'){
+      this.token = localStorage.getItem('token') || {}
+    }
 
     let head = new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('Access-Control-Allow-Origin', '*')
-        .set('Authorization', `Bearer ${token}`)
+        .set('Authorization', `Bearer ${this.token}`)
 
     return this.http.get<Quiz[]>(`${this.urlBase}/questionnaire?age=${age}`, { headers: head })
   }
 
   public addQuizAnswers(answersList: StudentsQuestionnaire[]): Observable<any> {
 
-    const token = localStorage.getItem('token') || {}
+    if (typeof localStorage !== 'undefined'){
+      this.token = localStorage.getItem('token') || {}
+    }
 
     let head = new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('Access-Control-Allow-Origin', '*')
-        .set('Authorization', `Bearer ${token}`)
+        .set('Authorization', `Bearer ${this.token}`)
 
     return this.http.post<Quiz[]>(`${this.urlBase}/checklist`, answersList, { headers: head })
   }
 
   getReportAnswers(id_student: string) {
-    const token = localStorage.getItem('token') || {}
+    if (typeof localStorage !== 'undefined'){
+      this.token = localStorage.getItem('token') || {}
+    }
 
     let head = new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('Access-Control-Allow-Origin', '*')
-        .set('Authorization', `Bearer ${token}`)
+        .set('Authorization', `Bearer ${this.token}`)
 
     return this.http.get<ReportResult>(`${this.urlBase}/report?id=${id_student}`, { headers: head })
   }
 
   getReportChar(id_student: string) {
-    const token = localStorage.getItem('token') || {}
+    if (typeof localStorage !== 'undefined'){
+      this.token = localStorage.getItem('token') || {}
+    }
 
     let head = new HttpHeaders()
     .set('Content-Type', 'application/json')
     .set('Access-Control-Allow-Origin', '*')
-    .set('Authorization', `Bearer ${token}`)
+    .set('Authorization', `Bearer ${this.token}`)
 
     return this.http.get<Characteristics>(`${this.urlBase}/get-characteristics?id=${id_student}`, { headers: head })
   }
